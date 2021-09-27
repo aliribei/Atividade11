e781cd738a41fab14ed3db3d001b0bf920aea01c
Aline Ribeiro <aline.ribeiro@capgemini.com>
2021-09-27 13:23:53 -0300

    Adiciona estrutura de pastas, metodo main, metodo cabecalho e metodo exibirMenu

---
+++ src/atividade_v1/Main.java
@@ -0,0 +1,28 @@
+package atividade_v1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        cabecalho();
+        
+    }
+    static void cabecalho() {
+        System.out.println("\n\n");
+        System.out.println("+-------------------------------------------+");
+        System.out.println("+------------------Bem Vindo----------------+");
+        System.out.println("+-----------Bradesco Financiamentos---------+");
+        System.out.println("+-------------------------------------------+");
+    }
+    static void exibirMenu() {
+        System.out.println("\n\n");
+        System.out.println("+-------------------------------------------+");
+        System.out.println("|        Cadastro de produtos               |");
+        System.out.println("|          Escolha uma opção:               |");
+        System.out.println("+-------------------------------------------+");
+        System.out.println("| 1 - Crédito Pessoal                       |");
+        System.out.println("| 2 - Crédito Imobiliário                   |");
+        System.out.println("| 3 - Crédito Empresarial                   |");
+        System.out.println("| 4 - Empréstimo Consignado                 |");
+        System.out.println("+-------------------------------------------+");
+    } 
+}
